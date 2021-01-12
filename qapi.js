0be230ec79a47c70fd228ccf39601a63d4da4ba6
Goalof <68434460+Goalof@users.noreply.github.com>
2021-01-13 03:04:41 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffe0d1ca6aceb001f3246cf",
+				"5ffe0d1ca6aceb001f3246d1"
+			]
+		},
+		"5ffe0d1ca6aceb001f3246cf": {
+			"id": "5ffe0d1ca6aceb001f3246cf",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffe0d1ca6aceb001f3246d1": {
+			"id": "5ffe0d1ca6aceb001f3246d1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
